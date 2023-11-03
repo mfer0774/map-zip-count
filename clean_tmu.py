427e0fb20563cbb1c859eeda1a45938b28c64cd2
mfer0774 <mfer0774@gmail.com>
2023-11-03 10:36:34 -0500

    clean-up

--- clean_tmu.py
+++ clean_tmu.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def clean_and_prepare_data(csv_file_path):
     """
-    Cleans and prepares the ticket sales data.
+    Cleans and prepares the raw TMU ticket sales data.
 
     Parameters:
     - csv_file_path: str, the path to the CSV file containing the event data.
@@ -11,17 +11,17 @@ def clean_and_prepare_data(csv_file_path):
     - DataFrame with the cleaned and prepared data.
     """
 
-    # Load the data
+    # load the data
     data = pd.read_csv(csv_file_path)
 
-    # Filter rows for the specific event and select columns
+    # filter rows for the specific event and select columns
     filtered_data = data[data['seller'] == 'Jurassic Quest | Cross Insurance Arena | Portland, ME 807']
-    selected_columns = filtered_data[['zipCode', 'buyerEmail']]  # Assuming 'buyerEmail' is needed
+    selected_columns = filtered_data[['zipCode', 'buyerEmail']]  # TODO: decide on group by order (currently shows total tickets sold per zip)
 
-    # Format zip codes as strings with leading zeros
+    # format zip codes as strings with leading zeros
     selected_columns['zipCode'] = selected_columns['zipCode'].astype(str).str.zfill(5)
 
-    # Count the number of tickets per zip code
+    # count the number of tickets per zip code
     ticket_counts = selected_columns.groupby('zipCode').size().reset_index(name='Ticket_Count')
 
     return ticket_counts
